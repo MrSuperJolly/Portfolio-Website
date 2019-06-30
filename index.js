f80922f588d4746e930aea23ce1357f3a3b90e7a
= <mrsuperjolly@gmail.com>
2019-06-30 04:17:46 +0100

    test

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express();
-require('dotenv').config()
 const port = process.env.PORT || 3000;
 app.listen(port, () => (console.log(`listening at ${port}`)));
 app.use(express.static('public'));
