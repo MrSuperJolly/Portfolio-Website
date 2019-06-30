c1b665de48cd25776a67ed54cd80819e9dae1c4c
= <mrsuperjolly@gmail.com>
2019-06-30 04:35:45 +0100

    capitalised p

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000;
 app.listen(port, () => (console.log(`listening at ${port}`)));
-app.use(express.static('public'));
+app.use(express.static('Public'));
